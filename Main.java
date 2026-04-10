9dcd2e6905f46267550fc0996a4eb45f41b3f624
Bárbara Chalhub <barbarachalhub1@gmail.com>
2026-04-10 13:08:16 -0300

    primeiro commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) { // comentário de uma linha
+        Scanner scanner = new Scanner(System.in); //System.in significa que a informação vem do teclado
+        System.out.println("Olá, informe seu nome:"); //o println já pula uma linha, o print normal não pula linha
+        String name = scanner.next(); //isso é uma variável
+        System.out.println("Informe sua idade:");
+        int age = scanner.nextInt(); //isso é uma variável
+        System.out.println("Olá " + name + " sua idade é " + age + " anos."); // existem formas diferentes de fazer esse print, segue próxima opção:
+        System.out.printf("Olá %s sua idade é %s anos.\n", name, age); //repare que agora é printf, o %s é um argumento
+
+        // outra forma de declarar variáveis:
+
+        System.out.println("Olá, informe seu nome:"); 
+        var nome = scanner.next(); //isso é uma variável, troquei name por nome para não dar conflito
+        System.out.println("Informe sua idade:");
+        var idade = scanner.nextInt(); //isso é uma variável, troquei age por idade para não dar conflito
+        System.out.println("Olá " + nome + " sua idade é " + idade + " anos."); 
+        
+
+    }
+
+}
+
+/*
+
+Comentário de várias linhas
+
+*/
